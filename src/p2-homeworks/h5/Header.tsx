01dc4558f988c7dfedd266539bd40c2e4a55a148
Igor Alekhin <alekhinigor7@gmail.com>
2022-04-03 18:24:15 +0300

    five homework finished

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,24 +1,18 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom';
 import {PATH} from "./Routes";
+import s from "./Header.module.css"
 
 function Header() {
     return (
         <div>
 
-            <nav>
-                <div>
-                    <NavLink to={"/"}></NavLink>
-                </div>
-                <div>
-                    <NavLink to={PATH.PRE_JUNIOR}>Pre-junior</NavLink>
-                </div>
-                <div>
-                    <NavLink to={PATH.JUNIOR}>Junior</NavLink>
-                </div>
-                <div>
-                    <NavLink to={PATH.JUNIOR_PLUS}>JuniorPlus</NavLink>
-                </div>
+            <nav className={s.header}>
+                <div><NavLink to={"/"} ></NavLink></div>
+                <div><NavLink to={PATH.PRE_JUNIOR} className={s.link}>Pre-junior</NavLink></div>
+                <div><NavLink to={PATH.JUNIOR} className={s.link}>Junior</NavLink></div>
+                <div><NavLink to={PATH.JUNIOR_PLUS} className={s.link}>JuniorPlus</NavLink></div>
+                <div className={s.block}></div>
             </nav>
 
         </div>
