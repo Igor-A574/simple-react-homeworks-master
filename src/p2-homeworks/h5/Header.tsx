ba5425a11e44bbed3d939a36106e17ce403a766d
Igor Alekhin <alekhinigor7@gmail.com>
2022-04-03 16:02:46 +0300

    five homework

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom';
+import {PATH} from "./Routes";
 
 function Header() {
     return (
         <div>
-            // add NavLinks
+
+            <nav>
+                <div>
+                    <NavLink to={"/"}></NavLink>
+                </div>
+                <div>
+                    <NavLink to={PATH.PRE_JUNIOR}>Pre-junior</NavLink>
+                </div>
+                <div>
+                    <NavLink to={PATH.JUNIOR}>Junior</NavLink>
+                </div>
+                <div>
+                    <NavLink to={PATH.JUNIOR_PLUS}>JuniorPlus</NavLink>
+                </div>
+            </nav>
 
         </div>
     )
---
+++ src/p2-homeworks/h5/pages/JuniorPlus.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const JuniorPlus = () => {
+    return (
+        <div>
+            JuniorPlus
+        </div>
+    )
+}
+
+
+
